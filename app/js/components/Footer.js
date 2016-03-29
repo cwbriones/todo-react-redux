db1fafc00d20239af6e92761c7bcaccc94ab0893
Christian Briones <cwbriones@gmail.com>
2016-03-28 23:55:31 -0700

    add state to Footer so the count updates properly

--- app/js/components/Footer.js
+++ app/js/components/Footer.js
@@ -1,23 +1,55 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import FilterLink from '../containers/FilterLink';
 
-const Footer = () => (
-  <footer className="footer">
-    <span className="todo-count">
-      <strong>2 items left.</strong>
-    </span>
-    <ul className="filters">
-      <FilterLink filter="SHOW_ALL">
-        All
-      </FilterLink>
-      <FilterLink filter="SHOW_ACTIVE">
-        Active
-      </FilterLink>
-      <FilterLink filter="SHOW_COMPLETED">
-        Completed
-      </FilterLink>
-    </ul>
-  </footer>
-);
+import { connect } from 'react-redux';
+
+function getTodosRemaining(todos) {
+  const remainingTodos = todos.filter(t => !t.completed);
+  return remainingTodos.length;
+}
+
+function mapStateToProps(state) {
+  const todos = state.todos.todoItems;
+  return {
+    remaining: getTodosRemaining(todos),
+    empty: todos.length === 0,
+  };
+}
+
+function RawFooter({ remaining, empty }) {
+  let style;
+  if (empty) {
+    style = { display: 'none' };
+  } else {
+    style = {};
+  }
+  return (
+    <footer className="footer" style={style}>
+      <span className="todo-count">
+        <strong>{remaining} item{remaining === 1 ? '' : 's'} left.</strong>
+      </span>
+      <ul className="filters">
+        <FilterLink filter="SHOW_ALL">
+          All
+        </FilterLink>
+        <FilterLink filter="SHOW_ACTIVE">
+          Active
+        </FilterLink>
+        <FilterLink filter="SHOW_COMPLETED">
+          Completed
+        </FilterLink>
+      </ul>
+    </footer>
+  );
+}
+
+RawFooter.propTypes = {
+  remaining: PropTypes.number.isRequired,
+  empty: PropTypes.bool.isRequired,
+};
+
+const Footer = connect(
+  mapStateToProps
+)(RawFooter);
 
 export default Footer;
